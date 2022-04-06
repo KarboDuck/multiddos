c65bc579a8dc21250da4d619883eb3ef4b3beeed
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-06 20:32:31 +0300

    Update multiddos_matrix.sh

--- multiddos_matrix.sh
+++ multiddos_matrix.sh
@@ -2,16 +2,16 @@
 sudo apt-get update -y
 sudo apt-get install -y tmux wget torsocks cmatrix python3 python3-pip gcc libc-dev libffi-dev libssl-dev python3-dev rustc
 pip install --upgrade pip
+#install docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install docker-ce -y
+    
 if [ ! -f "/usr/local/bin/gotop" ]; then
     wget https://github.com/cjbassi/gotop/releases/download/3.0.0/gotop_3.0.0_linux_amd64.deb
     sudo dpkg -i gotop_3.0.0_linux_amd64.deb
     rm gotop_3.0.0_linux_amd64.deb
-fi
-if [ ! -f "/usr/bin/docker" ]; then
-    sudo apt install apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-    sudo apt install docker-ce
 fi
 
 tmux kill-session -t multiddos
