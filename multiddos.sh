bea2efb1a49f7b2270c1308a8dbd5f6af26c1c97
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-07 01:14:45 +0300

    Update multiddos.sh

--- multiddos.sh
+++ multiddos.sh
@@ -3,7 +3,7 @@ sudo apt-get update -y
 sudo apt-get install -y tmux wget torsocks python3 python3-pip gcc libc-dev libffi-dev libssl-dev python3-dev rustc
 pip install --upgrade pip
 #install docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt install docker-ce -y
