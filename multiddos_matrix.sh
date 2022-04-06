cc4e5390886ff68ac2ddde327b449456491ee28f
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-06 20:32:52 +0300

    Update multiddos_matrix.sh

--- multiddos_matrix.sh
+++ multiddos_matrix.sh
@@ -33,6 +33,5 @@ tmux split-window -v 'torsocks -i ./db1000n'
 sleep 0.1
 tmux select-pane -t 0
 tmux split-window -v -p 33 'cmatrix'
-sleep 0.1
 #tmux split-window -v 'docker run --rm -it --pull always ghcr.io/arriven/db1000n'
 tmux -2 attach-session -d
