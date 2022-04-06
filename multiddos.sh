0b6a06d3a3096a1599d8cf8e4930a85894629aa9
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-06 20:25:21 +0300

    Update multiddos.sh

--- multiddos.sh
+++ multiddos.sh
@@ -11,7 +11,7 @@ if [ ! -f "/usr/bin/docker" ]; then
     sudo apt install apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-    sudo apt install docker-ce
+    sudo apt install docker-ce -y
 fi
 
 tmux kill-session -t multiddos
