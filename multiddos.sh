f081f96932361936fddf1385ba63a5c3c3a91bb5
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-10 12:20:04 +0300

    Update multiddos.sh

--- multiddos.sh
+++ multiddos.sh
@@ -7,6 +7,7 @@ pip install --upgrade pip
 #sudo apt install apt-transport-https ca-certificates curl software-properties-common
 #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 #sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+#sudo apt update
 #sudo apt install docker-ce -y
 
 # tmux mouse support
