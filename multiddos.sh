cfd9edf22ccbf5e6432d1ed7e53a8684a86c0575
uaripper <uaripper@mail.local>
2022-06-21 13:19:42 +0300

    remove git clone mhddos; update mhddos_proxy every 30 minutes

--- multiddos.sh
+++ multiddos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # curl -O https://raw.githubusercontent.com/KarboDuck/multiddos/main/md2.sh && bash md2.sh
-clear && echo -e "Loading... test v0.9.1\n"
+clear && echo -e "Loading... v0.9.1\n"
 sudo apt-get update -q -y #>/dev/null 2>&1
 sudo apt-get install -q -y tmux toilet python3 python3-pip 
 pip install --upgrade pip >/dev/null 2>&1
