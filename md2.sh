4a328081b10580f5ededc0e1d74b5b3fcff41222
uaripper <uaripper@mail.local>
2022-07-21 13:32:10 +0300

    adding new mode

--- md2.sh
+++ md2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # curl -LO tiny.one/multiddos && bash multiddos
 # curl -O https://raw.githubusercontent.com/KarboDuck/multiddos/main/md2.sh && bash md2.sh
-clear && echo -e "Loading... v1.21b\n"
+clear && echo -e "Loading... v1.21c\n"
 sudo apt-get update -q -y #>/dev/null 2>&1
 sudo apt-get install -q -y tmux jq git toilet python3 python3-pip 
 pip install --upgrade pip >/dev/null 2>&1
@@ -64,10 +64,10 @@ toilet -t --metal "   жнець"
 toilet -t --metal " MULTIDDOS"
 
 if [[ $mhddos_mode == "new" ]]; then
+    typing_on_screen 'Шукаю завдання від IT ARMY...' && sleep 2&
     cd ~/multidd/
-    wget -q https://github.com/porthole-ascend-cinnamon/mhddos_proxy_releases/releases/latest/download/mhddos_proxy_linux && chmod +x mhddos_proxy_linux&
-    typing_on_screen 'Шукаю завдання від IT ARMY...' ; sleep 1
-
+    wget -q https://github.com/porthole-ascend-cinnamon/mhddos_proxy_releases/releases/latest/download/mhddos_proxy_linux
+    chmod +x mhddos_proxy_linux
 else
     typing_on_screen 'Шукаю завдання...' ; sleep 0.5
     echo -e "\n\nTotal targets found:" "\x1b[32m $(cat ~/multidd/targets/all_targets.txt | wc -l)\x1b[m" && sleep 0.1
